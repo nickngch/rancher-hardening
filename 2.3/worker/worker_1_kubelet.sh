ccd7f9ecf68b62120c9fb781f65111a716acfea4
Nick Ng <nickngch@gmail.com>
2019-11-22 20:10:36 +0800

    fixing worker_1

--- 2.3/worker/worker_1_kubelet.sh
+++ 2.3/worker/worker_1_kubelet.sh
@@ -3,7 +3,7 @@ info "2.1 - Kubelet"
 check_2_1_1="2.1.1  - Ensure that the --anonymous-auth argument is set to false"
 #if check_argument "$CIS_KUBELET_CMD" '--anonymous-auth=false' >/dev/null 2>&1; then
 anonymous=$(docker inspect kubelet | jq -e '.[0].Args[] | match("--anonymous-auth=false").string')
-if [ $anonymous = "--anonymous-auth=false" ]; then
+if [ $anonymous = \"--anonymous-auth=false\" ]; then
   pass "$check_2_1_1"
 else
   warn "$check_2_1_1"
@@ -11,7 +11,7 @@ fi
 
 check_2_1_2="2.1.2  - Ensure that the --authorization-mode argument is not set to AlwaysAllow"
 authorization=$(docker inspect kubelet | jq -e '.[0].Args[] | match("--authorization-mode=Webhook").string')
-if [ $authorization = "--authorization-mode=Webhook" ]; then
+if [ $authorization = \"--authorization-mode=Webhook\" ]; then
   warn "$check_2_1_2"
 else
   pass "$check_2_1_2"
@@ -20,7 +20,7 @@ fi
 check_2_1_3="2.1.3  - Ensure that the --client-ca-file argument is set as appropriate"
 #if check_argument "$CIS_KUBELET_CMD" '--client-ca-file' >/dev/null 2>&1; then
 cafile=$(docker inspect kubelet | jq -e '.[0].Args[] | match("--client-ca-file=.*").string')
-if [ "$cafile" = "--client-ca-file=/etc/kubernetes/ssl/kube-ca.pem" ]; then
+if [ "$cafile" = \"--client-ca-file=/etc/kubernetes/ssl/kube-ca.pem\" ]; then
   pass "$check_2_1_3"
 else
   warn "$check_2_1_3"
@@ -29,7 +29,7 @@ fi
 
 check_2_1_4="2.1.4  - Ensure that the --read-only-port argument is set to 0"
 port=$(docker inspect kubelet | jq -e '.[0].Args[] | match("--read-only-port=0").string')
-if [ $port = "--read-only-port=0" ]; then
+if [ $port = \"--read-only-port=0\" ]; then
   pass "$check_2_1_4"
 else
   warn "$check_2_1_4"
@@ -37,7 +37,7 @@ fi
 
 check_2_1_5="2.1.5  - Ensure that the --streaming-connection-idle-timeout argument is not set to 0"
 timeout=$(docker inspect kubelet | jq -e '.[0].Args[] | match("--streaming-connection-idle-timeout=.*").string')
-if [ $timeout = "--streaming-connection-idle-timeout=1800s" ]; then
+if [ $timeout = \"--streaming-connection-idle-timeout=1800s\" ]; then
   pass "$check_2_1_5"
 else
   warn "$check_2_1_5"
@@ -46,7 +46,7 @@ fi
 
 check_2_1_6="2.1.6  - Ensure that the --protect-kernel-defaults argument is set to true"
 pks=$(docker inspect kubelet | jq -e '.[0].Args[] | match("--protect-kernel-defaults=true").string')
-if [ "$pks" = "--protect-kernel-defaults=true" ]; then
+if [ "$pks" = \"--protect-kernel-defaults=true\" ]; then
     pass "$check_2_1_6"
 else
     warn "$check_2_1_6"
@@ -54,7 +54,7 @@ fi
 
 check_2_1_7="2.1.7  - Ensure that the --make-iptables-util-chains argument is set to true"
 iptables=$(docker inspect kubelet | jq -e '.[0].Args[] | match("--make-iptables-util-chains=true").string')
-if [ $iptables = "--make-iptables-util-chains=true" ]; then
+if [ $iptables = \"--make-iptables-util-chains=true\" ]; then
     pass "$check_2_1_7"
 else
     warn "$check_2_1_7"
@@ -70,7 +70,7 @@ fi
 
 check_2_1_9="2.1.9  - Ensure that the --event-qps argument is set to 0"
 event=$(docker inspect kubelet | jq -e '.[0].Args[] | match("--event-qps=0").string')
-  if [ $event = "--event-qps=0" ]; then
+  if [ $event = \"--event-qps=0\" ]; then
     pass "$check_2_1_9"
   else
     warn "$check_2_1_9"
@@ -81,7 +81,7 @@ check_2_1_10="2.1.10  - Ensure that the --tls-cert-file and --tls-private-key-fi
 tcf=$(docker inspect kubelet | jq -e '.[0].Args[] | match("--tls-cert-file=.*").string')
 pkf=$(docker inspect kubelet | jq -e '.[0].Args[] | match("--tls-private-key-file=.*").string')
 if [ -z "$tcf" ]; then
-  if [ -z "pkf" ]; then
+  if [ -z "$pkf" ]; then
     pass "$check_2_1_10"
     pass "        * tls-cert-file: $tcf"
     pass "        * tls-private-key-file: $pkf"
@@ -133,47 +133,47 @@ jj=$(docker inspect kube-apiserver | jq -e '.[0].Args[] | match("--tls-cipher-su
 count_check=0
 if [ "$aa" != "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256" ]; then
 	count_check=$(( count_check + 1 ))
-	warn "$check_2_1_14"
+	warn "$aa not found"
 fi
 if [ "$bb" != "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256" ]; then
         count_check=$(( count_check + 1 ))
-        warn "$check_2_1_14"
+        warn "$bb not found"
 fi
 if [ "$cc" = "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305" ]; then
         count_check=$(( count_check + 1 ))
-	warn "$check_2_1_14"
+	warn "$cc not found"
 fi
 if [ "$dd" = "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384" ]; then
         count_check=$(( count_check + 1 ))
-	warn "$check_2_1_14"
+	warn "$dd not found"
 fi
 if [ "$ee" = "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305" ]; then
         count_check=$(( count_check + 1 ))
-	warn "$check_2_1_14"
+	warn "$ee not found"
 fi
 if [ "$ff" = "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384" ]; then
         count_check=$(( count_check + 1 ))
-	warn "$check_2_1_14"
+	warn "$ff not found"
 fi
 if [ "$gg" = "TLS_RSA_WITH_AES_256_GCM_SHA384" ]; then
         count_check=$(( count_check + 1 ))
-	warn "$check_2_1_14"
+	warn "$gg not found"
 fi
 if [ "$hh" = "TLS_RSA_WITH_AES_128_GCM_SHA256" ]; then
         count_check=$(( count_check + 1 ))
-	warn "$check_2_1_14"
+	warn "$hh not found"
 fi
 if [ -z "$ii" ]; then
 	sleep 1
 else
         count_check=$(( count_check + 1 ))
-	warn "$check_2_1_14"
+	warn "$ii is enabled"
 fi
 if [ -z "$jj" ]; then
 	sleep 1
 else
         count_check=$(( count_check + 1 ))
-	warn "$check_2_1_14"
+	warn "$jj is enabled"
 fi
 if [ "$count_check" -ge 1 ];then
 	warn "$check_2_1_14"
